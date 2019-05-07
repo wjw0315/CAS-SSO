12b5605d12d63f564f906ab5f4bff804db8b3ef0
wjw0215 <821060818@qq.com>
2019-05-07 19:29:56 +0800

    update

---
+++ sos-config/src/test/java/com/wjwcloud/auth/sosconfig/SosConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wjwcloud.auth.sosconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SosConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
